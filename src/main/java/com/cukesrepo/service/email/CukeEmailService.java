591852c15911ba53826832e81a5161649ecc5bc1
kugajjar <kugajjar@paypal.com>
2014-10-22 00:19:35 -0400

    c3 fix

--- src/main/java/com/cukesrepo/service/email/CukeEmailService.java
+++ src/main/java/com/cukesrepo/service/email/CukeEmailService.java
@@ -4,7 +4,9 @@ import com.cukesrepo.domain.Feature;
 import com.cukesrepo.domain.Project;
 import com.cukesrepo.exceptions.EmailException;
 
-public interface EmailService {
+
+public interface CukeEmailService
+{
 
     public String sendReviewRequest(Project project, Feature feature) throws EmailException;
 }
--- src/main/java/com/cukesrepo/service/email/CukeEmailServiceImpl.java
+++ src/main/java/com/cukesrepo/service/email/CukeEmailServiceImpl.java
@@ -17,18 +17,19 @@ import org.apache.commons.lang.Validate;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 
-@org.springframework.stereotype.Service
-public class EmailServiceImpl implements EmailService
+@Service
+public class CukeEmailServiceImpl implements CukeEmailService
 {
 
     private final EmailComponent _emailComponent;
 
-    private static final Logger LOG = LoggerFactory.getLogger(EmailServiceImpl.class);
+    private static final Logger LOG = LoggerFactory.getLogger(CukeEmailServiceImpl.class);
 
     @Autowired
-    public EmailServiceImpl(EmailComponent emailComponent)
+    public CukeEmailServiceImpl(EmailComponent emailComponent)
     {
 
         Validate.notNull(emailComponent, "emailComponent cannot be null");
