b484816cf5dd6b3b93f5d6639e2c0434121debc8
kugajjar <kugajjar@paypal.com>
2014-10-23 23:24:43 -0400

    update project

--- src/main/java/com/cukesrepo/page/UpdateProjectPage.java
+++ src/main/java/com/cukesrepo/page/UpdateProjectPage.java
@@ -51,7 +51,7 @@ public class UpdateProjectPage extends HeaderFooter implements Renderable
             html.br();
             html.br();
             html.div(class_("addproject-div-title"));
-            html.h2().span(id("update-add-title")).content("update project").br().br();
+            html.h2().span(id("update-add-title")).content("update project").br();
 
             html.span(id("update-error")).content("");
             html.input(type("text").class_("add-project project-name").value(project.getId()).add("placeholder", "Git Project Name").disabled("")).br().br();
