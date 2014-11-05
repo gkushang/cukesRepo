c2cadc77d2dd319ff28c39823c69b8a81ce88835
kugajjar <kugajjar@paypal.com>
2014-11-04 19:16:43 -0500

    add project

--- src/main/webapp/WEB-INF/resources/scripts/addproject.js
+++ src/main/webapp/WEB-INF/resources/scripts/addproject.js
@@ -13,20 +13,20 @@ jQuery(document).ready(function() {
 
              $.ajax({
 
-                url: "/user/add-project/persist",
-                data:{projectname: project_name,
-                displayprojectname: display_project_name,
-                repositorypath: repository_url,
-                featurespath: path_to_features,
-                emailofpo: email_of_po,
-                collaborators: collaborators_email},
+                        url: "/user/add-project/persist",
 
+                        data:{
+                                 projectname: project_name,
+                                 displayprojectname: display_project_name,
+                                 repositorypath: repository_url,
+                                 featurespath: path_to_features,
+                                 emailofpo: email_of_po,
+                                 collaborators: collaborators_email
+                             },
 
-                    }).done(function(data) {
+                     }).done(function(data) {
 
                     top.location.href = "/projects/";
-
-                                                          });
-
+                                     });
             });
 });
